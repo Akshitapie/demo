a0affa6e8eff90a4b7470b2b340ad03c5e910ccf
Akshitapie <166495864+Akshitapie@users.noreply.github.com>
2024-06-12 12:34:07 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,17 +1,19 @@
-# example/views.py
 from datetime import datetime
 
 from django.http import HttpResponse
 
-
 def index(request):
-    now = datetime.now()
-    html = f"""
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    """
-    return HttpResponse(html)
+    try:
+        now = datetime.now()
+        html = f"""
+        <html>
+            <body>
+                <h1>Hello from Vercel!</h1>
+                <p>The current time is { now }.</p>
+            </body>
+        </html>
+        """
+        return HttpResponse(html)
+    except Exception as e:
+        error_message = "An error occurred while processing the request."
+        return HttpResponse(error_message, status=500)
